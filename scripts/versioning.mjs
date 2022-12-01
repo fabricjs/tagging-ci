64449c8900666bce9c09066d7534735c529bd784
ShaMan123 <shacharnen@gmail.com>
2022-12-01 19:11:57 +0200

    Update versioning.mjs

--- scripts/versioning.mjs
+++ scripts/versioning.mjs
@@ -1,17 +1,17 @@
-import { execSync } from 'child_process';
-import fs from 'fs';
-import semver from 'semver';
+import { execSync } from "child_process";
+import fs from "fs";
+import semver from "semver";
 
-const LABEL_PREFIX = 'release:';
+const LABEL_PREFIX = "release:";
 
 export async function bumpPRVersion(context) {
   const version = process.env.BASE_VERSION;
   const { version: headVersion } = JSON.parse(
-    fs.readFileSync('../package.json')
+    fs.readFileSync("./package.json")
   );
 
-  const blockingLabels = process.env.STRICT_NO_RELEASE_LABELS.split('\n');
-  const nonBlockingLabels = process.env.NO_RELEASE_LABELS.split('\n');
+  const blockingLabels = process.env.STRICT_NO_RELEASE_LABELS.split("\n");
+  const nonBlockingLabels = process.env.NO_RELEASE_LABELS.split("\n");
   const { labels } = context.payload.pull_request;
 
   // get version level from PR label
@@ -21,10 +21,10 @@ export async function bumpPRVersion(context) {
   let shouldBump = !labels.some((label) => blockingLabels.includes(label.name));
   if (releaseLabels.length > 1 && shouldBump) {
     throw new Error(
-      `Found more than one release label: ${releaseLabels.join(', ')}`
+      `Found more than one release label: ${releaseLabels.join(", ")}`
     );
   }
-  const releaseLevel = releaseLabels[0]?.replace(LABEL_PREFIX, '');
+  const releaseLevel = releaseLabels[0]?.replace(LABEL_PREFIX, "");
   if (!releaseLevel && shouldBump) {
     shouldBump = !labels.some((label) =>
       nonBlockingLabels.includes(label.name)
@@ -34,24 +34,24 @@ export async function bumpPRVersion(context) {
   const nextVersion = shouldBump
     ? semver.inc(
         version,
-        releaseLabels[0]?.replace(LABEL_PREFIX, '') || 'prerelease',
+        releaseLabels[0]?.replace(LABEL_PREFIX, "") || "prerelease",
         process.env.PRERELEASE_TAG
       )
     : version;
 
   // write file
   fs.writeFileSync(
-    './package.json',
+    "./package.json",
     JSON.stringify(
       {
-        ...JSON.parse(fs.readFileSync('../package.json')),
+        ...JSON.parse(fs.readFileSync("./package.json")),
         version: nextVersion,
       },
       null,
       2
     )
   );
-  execSync('npx prettier --write package.json');
+  execSync("npx prettier --write package.json");
 
   if (nextVersion !== headVersion) {
     console.log(
@@ -60,6 +60,6 @@ export async function bumpPRVersion(context) {
     return nextVersion;
   } else {
     console.log(`Version is up to date (${headVersion})`);
-    return '';
+    return "";
   }
 }
